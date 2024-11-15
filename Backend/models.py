41e61c25c3d60363bdee395db7664c1b8a8cecb2
Prajwal Devaraj <pdevaraj@kent.edu>
2024-11-14 20:51:54 -0500

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -1,1 +1,13 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['inventory_db']
+
+def add_item(name, stock_level, reorder_level):
+    item = {
+        'name': name,
+        'stock_level': stock_level,
+        'reorder_level': reorder_level
+    }
+    db.inventory.insert_one(item)
